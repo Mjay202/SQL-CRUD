73e6a37ff0ead28d18fed32d415ef58bda9cbd1b
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-01-19 18:11:28 +0100

    added book list template

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mysql from 'mysql2';
+import cors from 'cors'
 
 
 const app = express();
 
-
+app.use(cors())
 app.use(express.json());
 
 const db = mysql.createConnection({
